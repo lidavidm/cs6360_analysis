bf7c48248123f54c750aa0370a09eb613fa75eef
David Li <li.davidm96@gmail.com>
2016-05-14 20:38:39 -0400

    feat: add SQL query to get completion times

---
+++ completion_time.sql
@@ -0,0 +1,17 @@
+USE cs_6360_spring2016;
+SELECT
+        user_id, SUM(level_end_timestamp - log_quest_ts)
+FROM player_quest_log
+WHERE
+        level_end_timestamp IS NOT NULL AND
+        game_id = 4 AND
+        user_id in (
+                SELECT
+                        user_id
+                FROM player_action_log
+                WHERE
+                        game_id = 4 AND
+                        quest_id = -20 AND
+                        log_timestamp >= unix_timestamp('2016-05-09')
+                )
+GROUP BY user_id;
